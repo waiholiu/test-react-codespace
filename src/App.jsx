0e813f98470e2877265d50fe53a08a908566af0c
Wai Liu <waiholiu@users.noreply.github.com>
2023-11-28 05:45:32 +0000

    first change

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Welcome to the Codespace Demo!
           </a>
         </p>
       </header>
